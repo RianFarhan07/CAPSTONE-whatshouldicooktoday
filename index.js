8a10f092b6cd1cbc2c4ff1cf91450a3f00311cff
RianFarhan07 <rian.mallanti@gmail.com>
2025-02-08 08:53:40 +0800

    test

--- index.js
+++ index.js
@@ -12,22 +12,25 @@ const API_URL =
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
-app
-  .use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-  .get("/", async (req, res) => {
-    try {
-      const result = await axios.get(API_URL + "random.php");
-      console.log(result.data.meals[0]);
-      res.render("index.ejs", {
-        data: result.data.meals[0],
-      });
-    } catch (error) {
-      res.render("index.ejs", {
-        error: error,
-      });
-    }
-  });
+app.get("/", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "random.php");
+    console.log(result.data.meals[0]);
+    res.render("index.ejs", {
+      data: result.data.meals[0],
+    });
+  } catch (error) {
+    res.render("index.ejs", {
+      error: error,
+    });
+  }
+});
+
+app.get("/test", (req, res) => {
+  res.send("test");
+});
 
 app.listen(port, () => {
   console.log(`server are running on port ${port}`);
